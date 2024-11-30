c4b327de15efa13cef72f909c649cb9f21511c69
erbhuvnesh <52860707+erbhuvnesh@users.noreply.github.com>
2024-11-30 22:44:48 +0530

    status enum value fix

--- app/models/task.model.js
+++ app/models/task.model.js
@@ -4,7 +4,7 @@ const TaskSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   title: { type: String, required: true, unique: true },
   description: { type: String },
-  status: { type: String, enum: ["inprogress", "completed", "pending"], required: true },
+  status: { type: String, enum: ["inprogress", "todo", "done"], required: true },
   priority: { type: String, enum: ["low", "medium", "high"], default: "low" }, // Optional with default
   dueDate: { type: Date }, // Optional
   created: { type: Date, default: Date.now },
