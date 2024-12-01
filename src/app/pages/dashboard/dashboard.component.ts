e107a6a187aa72521806aff62e3176f033eef96a
erbhuvnesh <52860707+erbhuvnesh@users.noreply.github.com>
2024-12-01 19:45:06 +0530

    paginator issue fix

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnChanges, OnInit, ViewChild } from "@angular/core";
+import { Component, OnInit, ViewChild } from "@angular/core";
 import { TaskService } from "../../services/task.service";
 import { Task } from "../../models/task.model";
 import { MatDialog } from "@angular/material/dialog";
